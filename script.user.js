b4e6f6fa59c025d4916b509499c0f377aecfa5f2
eliogos <harshsandwich@gmail.com>
2026-02-20 12:32:30 +0800

    bump version

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ClickUp --update Modal
 // @namespace    clickup-update-modal
-// @version      10
+// @version      10.1
 // @description  Insert update template in ClickUp
 // @match        https://app.clickup.com/*
 // @grant        GM_getResourceText
--- src/constants.js
+++ src/constants.js
@@ -7,7 +7,7 @@
     global.GM_info.script &&
     global.GM_info.script.version
       ? String(global.GM_info.script.version)
-      : "10";
+      : "10.1";
 
   app.constants = {
     APP_VERSION: scriptVersion,
